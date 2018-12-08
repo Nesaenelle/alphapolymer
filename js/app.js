7294d709a0a14475b386992208807f95b24b0850
Bogdan Zinkevych <venomsunset@gmail.com>
2018-12-08 15:50:35 +0200

    fix

--- js/app.js
+++ js/app.js
@@ -1,20 +1,73 @@
-Vue.filter('translate', function (value) {
-  if (!value) return ''
-  value = value.toString();
-  return value.charAt(0).toUpperCase() + value.slice(1)
+Vue.filter('translate', function(value) {
+    return value;
 })
+
+var languages = ['ru', 'ua', 'en']
+var langState = new rxjs.BehaviorSubject();
+
+function defaultLanguage() {
+    window.location.hash = 'ru';
+    langState.next('ru');
+}
+
+checkLanguage();
+
+function checkLanguage() {
+    if (!window.location.hash) {
+        defaultLanguage();
+    } else {
+        if (languages.indexOf(window.location.hash.substr(1)) > -1) {
+            langState.next(window.location.hash.substr(1));
+        } else {
+            defaultLanguage();
+        }
+    }
+}
+
+
+window.addEventListener("hashchange", function() {
+    checkLanguage()
+}, false);
 
 
 var app = new Vue({
     el: '#app',
     data: {
-       Resources: {}
+        Resources: {}
     },
     mounted() {
-    	 this.$http.get('/translate/ru.json').then(response => {
-    	 	this.Resources = response.body;
-    	 }, response => {
-    	
-    	 });
+        langState
+            .subscribe(res => {
+                if (!res) return;
+                if (res === 'ru') {
+                    this.$http.get('/translate/ru.json').then(response => {
+                        this.Resources = response.body;
+                    }, response => {
+
+                    });
+                }
+
+                if (res === 'en') {
+                    this.$http.get('/translate/en.json').then(response => {
+                        this.Resources = response.body;
+                    }, response => {
+
+                    });
+                }
+
+                if (res === 'ua') {
+                    this.$http.get('/translate/ua.json').then(response => {
+                        this.Resources = response.body;
+                    }, response => {
+
+                    });
+                }
+            });
+    },
+    methods: {
+        changeLang: function(lang) {
+            langState.next(lang);
+            window.location.hash = lang;
+        }
     }
 });
